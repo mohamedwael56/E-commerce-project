3238a19a6e1c446c3951351d231b4fd69e3cfdf8
mohamed wael <mohmedhomos0147@gmail.com>
2025-11-21 02:06:27 +0200

    putting the products in the body of the home page

--- ecommerce-project/src/Home/Home.jsx
+++ ecommerce-project/src/Home/Home.jsx
@@ -6,40 +6,40 @@ export function Home() {
   return (
     <>
       <div className="header">
-<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Store name</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Store name</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <form class="d-flex ms-auto mt-sm-3 mb-sm-2" role="search">
-        <input class="form-control me-2 pe-5 " type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <form className="d-flex ms-auto mt-sm-3 mb-sm-2" role="search">
+        <input className="form-control me-2  " type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
 
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li className="dropdown-divider"></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-                <li class="nav-item">
-          <a class="nav-link active" href="#">account</a>
+                <li className="nav-item">
+          <a className="nav-link active" href="#">account</a>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Orders</a>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Orders</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Cart</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Cart</a>
         </li>
       </ul>
     </div>
@@ -47,10 +47,32 @@ export function Home() {
 </nav>
       </div>
 
-      <div>
-        <h1>Welcome to the E-commerce Store</h1>
-        <p>Browse our collection of products and enjoy shopping!</p>
-      </div>
+<div className="d-flex justify-content-around flex-wrap  ">
+<div className="card" style={{width: "15rem", height: "60%"}}>
+  <img src="../../public/images/intermediate-composite-basketball.jpg" className="card-img-top  " alt="..."></img>
+  <div className="card-body">
+    <h5 className="card-title">intermediate composite basketball</h5>
+    <p className="card-text opacity-0">Added.</p>
+    <a href="#" className="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+<div className="card" style={{width: "15rem"}}>
+  <img src="../../public/images/men-brown-flat-sneakers.jpg" className="card-img-top" alt="..."></img>
+  <div className="card-body">
+    <h5 className="card-title">men brown flat sneakers</h5>
+    <p className="card-text opacity-0">Added.</p>
+    <a href="#" className="btn btn-primary mt-4">Add To Cart</a>
+  </div>
+</div>
+<div className="card" style={{width: "15rem"}}>
+  <img src="../../public/images/men-cozy-fleece-hoodie-light-teal.jpg" className="card-img-top" alt="..."></img>
+  <div className="card-body">
+    <h5 className="card-title">men cozy fleece hoodie light teal</h5>
+    <p className="card-text opacity-0">Added.</p>
+    <a href="#" className="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+</div>
     </>
   );
 }
