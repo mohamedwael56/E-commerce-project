02ef778801a3ce58e52df265712b4f7625a30a3a
mohamed wael <mohmedhomos0147@gmail.com>
2025-11-21 20:06:37 +0200

    create the layout  of the products in the cart  including the delivery date the delivery options and each product details

--- ecommerce-project/src/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/checkout/CheckoutPage.jsx
@@ -1,4 +1,6 @@
-import './CheckoutPage.css';
+import "./CheckoutPage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 export function CheckoutPage() {
   return (
     <>
@@ -9,7 +11,7 @@ export function CheckoutPage() {
               Ym store
             </a>
             <a className=" navbar-brand" href="#">
-              Checkout(0)
+              Checkout (2)
             </a>
             <a className="navbar-brand" href="#">
               orders
@@ -17,43 +19,149 @@ export function CheckoutPage() {
           </div>
         </nav>
       </div>
-<div className='container'>
-      <div className="payment-summary">
-        <div className="card" >
-          <div className="card-body ">
-            <h3 className="card-title ">payment summary</h3>
-            <div className="payment-summary-row">
-              <p className="card-subtitle mb-2 ">items(1):</p>
-              <div className="payment-money"> $7.30</div>
+      <div className="container">
+         <div className="product-card" >
+        <div className="card-body">
+          <h5 className="product-card-title">Delivery date: November 28</h5>
+          <div className="d-flex flex-row">
+            <img
+              src="../../public/images/intermediate-composite-basketball.jpg"
+              className=""
+              style={{ width: "120px", height: "120px" }}
+            ></img>
+                  <div className="product-details ">
+                <div className="product-name "> intermediate composite basketball</div>  
+
+                    <div className="product-price ">$7.30</div>
+<div className="quantity-actions">
+  <span className="product-quantity">quantity: 1</span>
+  <span className="link-primary">update</span>
+  <span className="link-primary">delete</span>
+</div>
+                    </div>          
+  </div>
             </div>
-            <div className="payment-summary-row">
-              <p className="card-text">shipping & handling:</p>
-              <div className="payment-money">$3.10</div>
+            <div className="delivery-options d-flex flex-column mt-3 mb-3">
+        <div className="delivery-option-title product-card-title">choose a delivery option </div>
+
+        <div className="form-check">
+  <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1"  />
+  <label className="form-check-label mb-2" htmlFor="radioDefault1">
+    <div>Friday, November 28</div>
+    <div> FREE Shipping</div>
+
+  </label>
+
+</div>
+<div className="form-check">
+  <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2"  />
+  <label className="form-check-label mb-2" htmlFor="radioDefault2">
+   <div> Monday, November 24</div>
+    <div> $5.99 Shipping</div>
+
+  </label>
+</div>
+<div className="form-check">
+  <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault3"   />
+  <label className="form-check-label mb-2" htmlFor="radioDefault3">
+    <div>Saturday, November 22</div>
+    <div> $9.99 Shipping</div>
+  </label>
+</div>
+
+</div>
+          </div>
+      
+        <div className="payment-summary">
+          <div className="card">
+            <div className="card-body ">
+              <h3 className="card-title ">payment summary</h3>
+              <div className="payment-summary-row">
+                <p className="card-subtitle mb-2 ">items(1):</p>
+                <div className="payment-money"> $7.30</div>
+              </div>
+              <div className="payment-summary-row">
+                <p className="card-text">shipping & handling:</p>
+                <div className="payment-money">$3.10</div>
+              </div>
+
+              <div className="payment-summary-row">
+                <p className="card-text">total before tax:</p>
+                <div className="payment-money"> $15.20</div>
+              </div>
+              <div className="payment-summary-row">
+                <p className="card-text">estimated tax:</p>
+                <div className="payment-money">$1.10</div>
+              </div>
+              <div className="payment-summary-row mt-2">
+                <p className="card-subtitle mb-2 ">order total:</p>
+                <div className="payment-money"> $20.00</div>
+              </div>
+              <button type="button" className="btn btn-primary mt-3">
+                Place your order
+              </button>
             </div>
-           
-            <div className="payment-summary-row">
-              <p className="card-text">total before tax:</p>
-              <div className="payment-money"> $15.20</div>
-            </div>
-            <div className="payment-summary-row">
-              <p className="card-text">estimated tax:</p>
-              <div className="payment-money">$1.10</div>
-            </div>
-            <div className="payment-summary-row mt-2">
-              <p className="card-subtitle mb-2 ">order total:</p>
-              <div className="payment-money"> $20.00</div>
-            </div>
-            <button type="button" className="btn btn-primary mt-3">
-              Place your order
-            </button>
           </div>
         </div>
-      </div>
 
+     
+                </div>
+<div className="container">
 
+                   <div className="second-product-card" >
+        <div className="second-card-body">
+          <h5 className="product-card-title">Delivery date: November 28</h5>
+          <div className="d-flex flex-row">
+            <img
+              src="../../public/images/men-brown-flat-sneakers.jpg"
+              className=""
+              style={{ width: "120px", height: "120px" }}
+            ></img>
+                  <div className="product-details ">
+                <div className="product-name "> men brown flat sneakers</div>  
 
+                    <div className="product-price ">$7.30</div>
+<div className="quantity-actions">
+  <span className="product-quantity">quantity: 1</span>
+  <span className="link-primary">update</span>
+  <span className="link-primary">delete</span>
+</div>
+                    </div>          
+  </div>
+            </div>
+            <div>
+            <div className="second-delivery-options ">
+        <div className="delivery-option-title product-card-title">choose a delivery option </div>
 
-      </div>
+        <div className="form-check">
+  <input className="form-check-input" type="radio" name="radioDefault2" id="radioDefault4"  />
+  <label className="form-check-label mb-2" htmlFor="radioDefault4">
+    <div>Friday, November 28</div>
+    <div> FREE Shipping</div>
+
+  </label>
+
+</div>
+<div className="form-check">
+  <input className="form-check-input" type="radio" name="radioDefault2" id="radioDefault5"  />
+  <label className="form-check-label mb-2" htmlFor="radioDefault5">
+   <div> Monday, November 24</div>
+    <div> $5.99 Shipping</div>
+
+  </label>
+</div>
+<div className="form-check">
+  <input className="form-check-input" type="radio" name="radioDefault2" id="radioDefault6"   />
+  <label className="form-check-label mb-2" htmlFor="radioDefault6">
+    <div>Saturday, November 22</div>
+    <div> $9.99 Shipping</div>
+  </label>
+</div>
+
+</div>
+</div>
+          </div>
+</div>
     </>
   );
 }
